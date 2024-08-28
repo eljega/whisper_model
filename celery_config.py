5925aaf79636b74e56dc13149602ef103654ad09
eljega <playgames.j.g17@gmail.com>
2024-08-27 22:34:05 -0500

    ahora si aplicando celery en el proyecto real

--- celery_config.py
+++ celery_config.py
@@ -1,11 +1,18 @@
 from celery import Celery
 
 def make_celery(app):
-    redis_url = 'redis://junction.proxy.rlwy.net:59093/0'
+    redis_url = 'redis://autorack.proxy.rlwy.net:16420/0'  # Actualiza con la URL correcta
     celery = Celery(
         app.import_name,
         backend=redis_url,
         broker=redis_url
     )
     celery.conf.update(app.config)
+
+    # Configuración de tiempos de espera
+    celery.conf.update({
+        'task_soft_time_limit': 3600,  # 1 hora para tareas largas
+        'task_time_limit': 7200,  # 2 horas como límite máximo
+    })
+
     return celery
