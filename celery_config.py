e43c115b3f3f204062ec723455d09d6d9c380fe9
eljega <playgames.j.g17@gmail.com>
2024-08-27 23:14:37 -0500

    prueba 2

--- celery_config.py
+++ celery_config.py
@@ -1,13 +1,11 @@
 from celery import Celery
 
 def make_celery(app):
-    redis_url = 'redis://redis.railway.internal:6379/0'
+    redis_url = 'redis://default:MTjiwpXRHbzKWTTbncmjutMjgLBOILrm@autorack.proxy.rlwy.net:16420/0'
     celery = Celery(
         app.import_name,
         backend=redis_url,
         broker=redis_url
     )
     celery.conf.update(app.config)
-    celery.conf.task_soft_time_limit = 1800  # 30 minutos
-    celery.conf.task_time_limit = 3600  # 60 minutos
     return celery
