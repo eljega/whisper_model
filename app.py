e1634b4a5ac92db48ca69ad265b3f8089fc633cf
eljega <playgames.j.g17@gmail.com>
2024-08-28 01:01:37 -0500

    prueba 8

--- app.py
+++ app.py
@@ -1,22 +1,17 @@
 from flask import Flask, request, jsonify
-import whisper
+import boto3
 import os
-from celery import Celery
+from celery_config import make_celery
+from botocore.exceptions import NoCredentialsError
 
-# Configuración de la aplicación Flask
 app = Flask(__name__)
-
-# Configuración de Redis para Celery
-redis_url = 'redis://default:MTjiwpXRHbzKWTTbncmjutMjgLBOILrm@autorack.proxy.rlwy.net:16420/0'
-celery = Celery(app.name, broker=redis_url, backend=redis_url)
-
-# Directorio donde se guardarán los archivos de video
-UPLOAD_FOLDER = '/app/uploads/'
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
-
-# Cargar el modelo Whisper
+celery = make_celery(app)
 model = whisper.load_model("base")
+
+# Configura tu cliente S3
+s3 = boto3.client('s3')
+
+BUCKET_NAME = 'clipgenaieljega'
 
 @app.route('/transcribe', methods=['POST'])
 def transcribe():
@@ -24,26 +19,31 @@ def transcribe():
         return jsonify({'error': 'No video file provided'}), 400
     
     video = request.files['video']
-    temp_video_path = os.path.join(UPLOAD_FOLDER, video.filename)
-
-    # Guardar el archivo y verificar si fue guardado correctamente
+    temp_video_path = os.path.join('/tmp', video.filename)
     video.save(temp_video_path)
-    
-    if not os.path.exists(temp_video_path):
-        return jsonify({'error': 'Failed to save video file'}), 500
-    
-    # Iniciar la tarea de transcripción
-    task = transcribe_video_task.delay(temp_video_path)
-    return jsonify({'task_id': task.id}), 202
+
+    # Subir el archivo a S3
+    try:
+        s3.upload_file(temp_video_path, BUCKET_NAME, video.filename)
+        s3_video_url = f"https://{BUCKET_NAME}.s3.amazonaws.com/{video.filename}"
+        os.remove(temp_video_path)  # Eliminar el archivo temporal localmente
+
+        task = transcribe_video_task.delay(s3_video_url)
+        return jsonify({'task_id': task.id}), 202
+    except NoCredentialsError:
+        return jsonify({'error': 'Credenciales de AWS no encontradas'}), 500
 
 @celery.task
-def transcribe_video_task(temp_video_path):
+def transcribe_video_task(s3_video_url):
+    # Descargar el archivo desde S3
+    temp_video_path = f"/tmp/{os.path.basename(s3_video_url)}"
+    s3.download_file(BUCKET_NAME, os.path.basename(s3_video_url), temp_video_path)
+
     if not os.path.exists(temp_video_path):
         raise RuntimeError(f"El archivo no existe: {temp_video_path}")
-    
-    # Transcribir el archivo de video
+
     result = model.transcribe(temp_video_path)
-    os.remove(temp_video_path)  # Eliminar el archivo después de la transcripción
+    os.remove(temp_video_path)
     return result['segments']
 
 @app.route('/task_status/<task_id>', methods=['GET'])
