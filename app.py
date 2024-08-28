28ab728d45cf013fa5e9d93ee90e48d8bd789d8b
eljega <playgames.j.g17@gmail.com>
2024-08-28 01:43:55 -0500

    prueba 12

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from celery_config import make_celery
 
 app = Flask(__name__)
 celery = make_celery(app)
-model = whisper.load_model("base")
+model = whisper.load_model("tiny")
 
 # Ruta del bucket en S3
 BUCKET_NAME = "clipgenaieljega"
