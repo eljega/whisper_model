6346b8ac8dab178c03eda8200847d162dd6cfe82
eljega <playgames.j.g17@gmail.com>
2024-08-27 23:28:16 -0500

    prueba 4

--- celery_config.py
+++ celery_config.py
@@ -8,4 +8,13 @@ def make_celery(app):
         broker=redis_url
     )
     celery.conf.update(app.config)
+    celery.conf.update(
+        task_serializer='json',
+        accept_content=['json'],
+        result_serializer='json',
+        timezone='UTC',
+        enable_utc=True,
+        task_time_limit=3600,  # 1 hora
+        task_soft_time_limit=3540,  # Un poco menos de 1 hora
+    )
     return celery
